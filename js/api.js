edb31c919965d41bab52f6db56cf957d8b38d37b
Damaris Cubos <damaris.cubos.rosas@gmail.com>
2023-03-24 20:43:21 -0600

    Add: createDom, main, api JS files

---
+++ js/api.js
@@ -0,0 +1,15 @@
+const BASE_URL = "https://kodemia-24js-default-rtdb.firebaseio.com";
+
+const getPosts = async () => {
+    let response = await fetch(`${BASE_URL}/POSTS/.json`);
+    let data = await response.json();
+    return data;
+};
+
+const getUsers = async () => {
+    let response = await fetch(`${BASE_URL}USERS/.json`);
+    let data = await response.json();
+    return data;
+};
+
+export { getPosts, getUsers };
