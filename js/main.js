ea850924e8a27190bc3ea8c0fd44262eaa494d10
Javier Briseno <jbf31293@gmail.com>
2023-03-29 18:53:34 -0600

    correct

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import { printRelevant, printLatest, printSearch } from "./post.js";
+import {
+    printRelevant,
+    printLatest,
+    printSearch
+} from "./post.js";
 
 let log = document.createElement("a")
 log.classList.add(..."btn btn-outline-primary me-2".split(" "))
@@ -16,33 +20,60 @@ image.classList.add("rounded-circle")
 image.setAttribute("src", "./assets/profile-avatar.webp")
 image.setAttribute("width", "40")
 image.setAttribute("heigth", "40")
-let collapse = document.createElement("a")
-collapse.setAttribute("href", "#collapse-menu")
-collapse.setAttribute("data-bs-toggle", "collapse")
-collapse.setAttribute("role", "button")
-collapse.appendChild(image)
 
 
+let navbarOptions = document.getElementById("rigthSide");
+let isLoggedIn = localStorage.getItem("token")
+let htmlContent = ``
 
+if (isLoggedIn) {
+  htmlContent = `
+<button class="crayons-header__menu__trigger position-absolute top-50 end-0 translate-middle-y me-5" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+  <span class="crayons-avatar crayons-avatar--l"><img alt="" id="avatarImage" class="crayons-avatar__image rounded-circle" src="./assets/profile-avatar.webp"></span>
+</button>
 
-const login = () => {
-    let container = document.getElementById("rigthSide")
-    let token = localStorage.getItem("token")
-    token ? container.append(post, collapse) : container.append(log, account)
+<div class="crayons-header__menu__dropdown dropdown-menu position-absolute top-100 start-100 translate-middle" aria-labelledby="dropdownMenuButton" id="userMenu">
+  <a class="dropdown-item" href="#">Dashboard</a>
+  <a class="dropdown-item" href="#">Settings</a>
+  <a class="dropdown-item" href="#">Reading list</a>
+  <li><hr class="dropdown-divider"></li>
+  <a class="dropdown-item" href="#" id="logout-link">Sign Out</a>
+</div>`
+} else {
+  htmlContent = `<div class="row">
+  <div class="col-4 position-absolute top-50 end-0 translate-middle-y">
+    <a href="./views/login.html" class="btn btn-outline-none">Log in</a>
+    <a class="btn btn-outline-none text-primary border border-primary">Create Account</a>
+  </div>
+  <div class="col-4">
+    
+  </div>
+</div>`
 }
 
-login()
+// Asignar el HTML al elemento de la navbar
+navbarOptions.innerHTML = htmlContent;
+
+/* const navbarLogin = () => {
+    let container = document.getElementById("rightSide")
+    let token = localStorage.getItem("token")
+    let tokenTrue = "<div class='dropdown'> <img src='avatar.png' alt='Avatar' class='dropdown-toggle' data-toggle='dropdown'> <div class='dropdown-menu'> <a class='dropdown-item' href='#'>Perfil</a> <a class='dropdown-item' href='#'>Configuración</a> <a class='dropdown-item' href='#'>Cerrar sesión</a> </div> </div> <span>Notificaciones</span> <span>Mensajes</span>"
+    let tokenFalse ="<span>Crear cuenta</span> <span>Iniciar sesión</span>"
+    let htmlContent
+    token ? htmlContent = tokenTrue : htmlContent = tokenFalse
+    container.innerHTML = htmlContent
+}
+
+navbarLogin() */
+
+/* const login = () => {
+    let container = document.getElementById("rigthSide")
+    let token = localStorage.getItem("token")
+    token ? container.append(post, image) : container.append(log, account)
+}
+ */
 // Pintar post relevantes al inicio
 printRelevant();
-
-
-const logoutLink = document.getElementById('logout-link');
-
-logoutLink.addEventListener('click', () => {
-    localStorage.removeItem('token');
-    window.location.replace('../index.html');
-});
-
 
 // Menus
 
@@ -77,3 +108,12 @@ btnSearch.addEventListener('click', () => {
     let searchTerm = inputSearch.value;
     printSearch(searchTerm);
 })
+
+
+//Logout Event Listener
+const logoutLink = document.getElementById('logout-link');
+
+logoutLink.addEventListener('click', () => {
+    localStorage.removeItem('token');
+    window.location.replace('../index.html');
+});
